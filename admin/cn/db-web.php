be97d640b19afc5ca433e4983b896625468e625c
GRUPOUTP\orojas <orojas@utp.edu.pe>
2024-05-08 12:56:39 -0500

    subiendo archivos

--- admin/cn/db-web.php
+++ admin/cn/db-web.php
@@ -1,9 +1,9 @@
 <?php 	
 	define('DB_HOST', '127.0.0.1');		
-	define('DB_USER','oerowe29_usoeAdmin_201');
-	define('DB_PASS','Adm-oe$201@2020$');
-	define('DB_NAME','oerowe29_oedb2020'); 
-	define('DB_PUERTO',3306); 
+	define('DB_USER','dtintoyb_admin01'); 
+	define('DB_PASS','A4m1n$01');
+	define('DB_NAME','dtintoyb_reservas'); 
+	define('DB_PUERTO',3306);
 
 	$db = mysqli_connect(DB_HOST, DB_USER,DB_PASS,DB_NAME,DB_PUERTO);
 	mysqli_query($db, "SET NAMES 'utf8'");
